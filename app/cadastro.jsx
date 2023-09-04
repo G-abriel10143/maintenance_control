f1a332bd3d47491fb02013fc09167dabd8942c97
Gabriel Cerqueira Alves <101301397+Gabriel0030@users.noreply.github.com>
2023-09-04 15:19:27 -0300

    cadastro esta pronto

---
+++ app/cadastro.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Cadastro from '/components/Cadastro';
+
+function CadastroPage() {
+  return (
+    <div>
+      <h1>Cadastro de Usuário</h1>
+      <Cadastro />
+    </div>
+  );
+}
+
+export default CadastroPage;
---
+++ components/cadastro.jsx
@@ -0,0 +1,45 @@
+import React, { useState } from 'react';
+
+function Cadastro() {
+  const [nome, setNome] = useState('');
+  const [email, setEmail] = useState('');
+
+  const handleNomeChange = (e) => {
+    setNome(e.target.value);
+  };
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Envie os dados para o servidor aqui
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="nome">Nome:</label>
+        <input
+          type="text"
+          id="nome"
+          value={nome}
+          onChange={handleNomeChange}
+        />
+      </div>
+      <div>
+        <label htmlFor="email">Email:</label>
+        <input
+          type="email"
+          id="email"
+          value={email}
+          onChange={handleEmailChange}
+        />
+      </div>
+      <button type="submit">Cadastrar</button>
+    </form>
+  );
+}
+
+export default Cadastro;
