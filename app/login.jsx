052ad33ddf60b6bc6e7e4404a1cdd92a1defcce3
Gabriel Cerqueira Alves <101301397+Gabriel0030@users.noreply.github.com>
2023-09-25 17:49:22 -0300

    fazendo update e delete

--- app/login.jsx
+++ app/login.jsx
@@ -8,6 +8,7 @@ const LoginPage = () => {
   const [password, setPassword] = useState('');
   const router = useRouter();
 
+  // Função para realizar o login
   const handleLogin = () => {
     // Simulação de lógica de autenticação básica
     if (username === 'user' && password === 'password') {
@@ -17,6 +18,27 @@ const LoginPage = () => {
     } else {
       alert('Credenciais inválidas.');
     }
+  };
+
+  // Função para realizar o cadastro de um novo usuário (operação de post)
+  const handleSignUp = () => {
+    // Aqui você pode implementar a lógica para enviar os dados do novo usuário para o servidor (API).
+    // Após o cadastro bem-sucedido, você pode redirecionar o usuário para a página de login ou realizar outra ação.
+    alert('Cadastro de novo usuário.');
+  };
+
+  // Função para atualizar a senha do usuário (operação de update)
+  const handleUpdatePassword = () => {
+    // Aqui você pode implementar a lógica para permitir que o usuário atualize sua senha.
+    // Geralmente, isso envolve enviar uma solicitação para o servidor (API) com a nova senha.
+    alert('Senha atualizada com sucesso.');
+  };
+
+  // Função para excluir a conta do usuário (operação de delete)
+  const handleDeleteAccount = () => {
+    // Aqui você pode implementar a lógica para permitir que o usuário exclua sua conta.
+    // Isso geralmente envolve uma confirmação e, em seguida, enviar uma solicitação para o servidor (API) para excluir a conta.
+    alert('Conta excluída com sucesso.');
   };
 
   return (
@@ -38,6 +60,15 @@ const LoginPage = () => {
         />
         <button type="button" onClick={handleLogin}>
           Entrar
+        </button>
+        <button type="button" onClick={handleSignUp}>
+          Cadastrar
+        </button>
+        <button type="button" onClick={handleUpdatePassword}>
+          Atualizar Senha
+        </button>
+        <button type="button" onClick={handleDeleteAccount}>
+          Excluir Conta
         </button>
       </form>
     </div>
